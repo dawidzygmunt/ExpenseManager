4500103347c3257fe9c6768a2db1cf335ff656ec
Dawid Zygmunt <dzygmunt@sii.pl>
2026-04-13 15:05:57 +0200

    LoginCommandHandler unit test

--- ExpensesManager.Api.UnitTest/Handlers/LoginCommandHandlerTests.cs
+++ ExpensesManager.Api.UnitTest/Handlers/LoginCommandHandlerTests.cs
@@ -1,5 +1,6 @@
 using ExpensesManager.Application.Commands;
 using ExpensesManager.Application.Handlers;
+using ExpensesManager.Application.Responses;
 using ExpensesManager.Domain.Entities;
 using ExpensesManager.Domain.Interfaces;
 using ExpensesManager.Infrastructure.Interfaces;
@@ -20,14 +21,61 @@ public class LoginCommandHandlerTests
     }
 
     [Fact]
+    public async Task LoginCommandHandler_Handle_WhenUserDoesNotExist_ThrowUnauthorizedAccessException()
+    {
+        // Arrange
+        var request = new LoginCommand("test@test.com", "password");
+        _userRepository
+            .Setup(x => x.GetByEmailAsync(request.Email))
+            .ReturnsAsync((User?)null);
+
+        // Act & Assert
+        await Assert.ThrowsAsync<UnauthorizedAccessException>(() => _sut.Handle(request, CancellationToken.None));
+    }
+
+    [Fact]
     public async Task LoginCommandHandler_Handle_WhenInvalidPassword_ThrowUnauthorizedAccessException()
     {
         // Arrange
         var request = new LoginCommand("test@test.com", "password");
+        var user = new User
+        {
+            Email = request.Email,
+        };
+
+        _userRepository.Setup(x => x.GetByEmailAsync(request.Email)).ReturnsAsync(user);
         _userRepository.Setup(x => x.CheckPasswordAsync(
-            It.Is<User>(u => u.Email == request.Email), request.Password));
-        
+            user, request.Password)).ReturnsAsync(false);
+
         // Act & Assert
-        await  Assert.ThrowsAsync<UnauthorizedAccessException>(() => _sut.Handle(request, CancellationToken.None));
+        await Assert.ThrowsAsync<UnauthorizedAccessException>(() => _sut.Handle(request, CancellationToken.None));
+    }
+
+    [Fact]
+    public async Task LoginCommandHandler_Handle_WhenCredentialsAreCorrect_ReturnLoginResponse()
+    {
+        // Arrange
+        var request = new LoginCommand("test@test.com", "password");
+        var user = new User
+        {
+            Email = request.Email,
+            UserName = request.Email,
+            FirstName = "mockedFirstName",
+            LastName = "mockedLastName",
+            PasswordHash = "mockedHashedPassword"
+        };
+        const string mockedAccessToken = "mockedAccessToken";
+
+        _userRepository.Setup(x => x.GetByEmailAsync(request.Email)).ReturnsAsync(user);
+        _userRepository.Setup(x => x.CheckPasswordAsync(
+            user, request.Password)).ReturnsAsync(true);
+
+        _jwtService.Setup(x => x.GenerateAccessToken(user)).Returns(mockedAccessToken);
+
+        // Act
+        var response = await _sut.Handle(request, CancellationToken.None);
+        
+        // Assert
+        Assert.Equal(mockedAccessToken, response.AccessToken);
     }
 }
